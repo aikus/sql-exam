87cc22fa9cfd0637263773f6c7f84ff7c6ea0c08
GermanovN <Nikita_Germanov@mail.ru>
2022-04-11 04:33:22 +0500

    Разрешение на редактирование только учителю (пока)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/')]
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_login', [], Response::HTTP_MOVED_PERMANENTLY);
+    }
+}
