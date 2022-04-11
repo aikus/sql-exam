65a64cdcdae2b12272b42fa2b26f88dad0b378e1
GermanovN <Nikita_Germanov@mail.ru>
2022-04-11 23:19:02 +0500

    Redirect

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -33,7 +33,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_exam');
+            return $this->redirectToRoute('app_exam_index');
         }
 
         return $this->render('registration/register.html.twig', [
