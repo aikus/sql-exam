0a4b61782546522cb87f61f2b1c5acd07c4e473b
GermanovN <Nikita_Germanov@mail.ru>
2022-07-06 20:08:06 +0500

    Добавил WebpackEncoreBundle #71

---
+++ assets/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Table, TableCell, TableRow} from "@mui/material";
+
+const App = () => {
+    return (
+        <Table>
+            <TableRow>
+                <TableCell>Task</TableCell>
+                <TableCell>Action</TableCell>
+            </TableRow>
+        </Table>
+    )
+}
+
+export default App;
