15ec94c2630aa4442d5b8eb452c88bd202ca249f
GermanovN <Nikita_Germanov@mail.ru>
2022-07-10 11:34:15 +0500

    Создал страницу для разработки фронта #71

---
+++ src/Controller/ReactDevPageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReactDevPageController extends AbstractController
+{
+    #[Route('/react', name: 'app_react_dev_page')]
+    public function index(): Response
+    {
+        return $this->render('react_api/index.html.twig', []);
+    }
+}
