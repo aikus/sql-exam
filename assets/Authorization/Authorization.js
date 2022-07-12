908a13de15d6c4fe46a17999479a82a837e46af0
tertyshnyi <tertyshnyiy.vladislav@gmail.com>
2022-07-12 22:33:16 +0500

    Работы над формой авторизации

--- assets/Authorization/Authorization.js
+++ assets/Authorization/Authorization.js
@@ -6,8 +6,10 @@ import logo from './img/logo.png'
 export const Authorization = () => {
     return (
         <C.Wrapper>
-            <img src={logo} alt="логотип"/>
-            <C.Header>Авторизация</C.Header>
+            <C.TopBlock>
+                <img src={logo} alt="логотип"/>
+                <C.Header>Авторизация</C.Header>
+            </C.TopBlock>
             <TextField
                 autoFocus
                 margin="normal"
@@ -25,6 +27,12 @@ export const Authorization = () => {
                 variant="outlined"
                 fullWidth
             />
+            <C.ForgotPassword href="#">Не помню пароль</C.ForgotPassword>
+            <C.Button>Войти</C.Button>
+            <C.RegistrationText>
+                <span>Нет учетной записи? </span>
+                <a href="#">Зарегистрироваться</a>
+            </C.RegistrationText>
         </C.Wrapper>
     )
 }
