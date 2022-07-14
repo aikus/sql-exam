49b330acd8c4345709923945861981ba996863a1
GermanovN <Nikita_Germanov@mail.ru>
2022-07-15 00:26:42 +0500

    Добавил бандл jwt #71

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
