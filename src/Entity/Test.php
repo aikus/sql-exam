aeea362388c9494aa55dc4555a112fa6336989a8
Никита Русаков <aikus@yandex.ru>
2022-09-06 05:33:26 +0500

    Добавил crud и api

--- src/Entity/Test.php
+++ src/Entity/Test.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\TestRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -9,6 +10,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TestRepository::class)]
+#[ApiResource]
 class Test
 {
     #[ORM\Id]
