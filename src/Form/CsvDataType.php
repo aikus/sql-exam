3051e024f7c17c9e66560c16248f5c61c68a1614
GermanovN <Nikita_Germanov@mail.ru>
2022-09-15 12:17:06 +0500

    Парсинг таблицы регультатов PR #90

---
+++ src/Form/CsvDataType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+
+class CsvDataType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('data', FileType::class, [
+                'label' => 'Data (CSV)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'text/csv',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid CSV document',
+                    ])
+                ],
+            ])
+        ;
+    }
+}
