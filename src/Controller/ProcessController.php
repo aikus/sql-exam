6686373fc6c1b78491601ef830c03d6e5007f098
GermanovN <Nikita_Germanov@mail.ru>
2022-10-09 00:49:32 +0500

    Следующий вопрос

--- src/Controller/ProcessController.php
+++ src/Controller/ProcessController.php
@@ -2,15 +2,14 @@
 
 namespace App\Controller;
 
-use App\Connectors\IdGenerator;
-use App\Entity\Answer;
 use App\Entity\Exam;
-use App\Entity\ExaminationSheet;
 use App\Entity\Question;
-use App\Repository\AnswerRepository;
-use App\Repository\ExaminationSheetRepository;
+use App\Service\ExaminationProcess\ExaminationProcess;
 use DateTime;
-use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Exception;
+use RusakovNikita\MysqlExam\Exam\Student;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,37 +18,39 @@ use Symfony\Component\Security\Core\Security;
 #[Route('/api-process')]
 class ProcessController extends AbstractController
 {
-
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     * @throws Exception
+     */
     #[Route('/{id}/start', name: 'app_exam_start', methods: ['GET'])]
-    public function start(Exam $exam,
-                          Security $security,
-                          IdGenerator $idGenerator,
-                          ExaminationSheetRepository $examinationSheetRepository,
-                          AnswerRepository $answerRepository
-    )
-    {
+    public function start(
+        Exam $exam,
+        Security $security,
+        ExaminationProcess $process
+    ): JsonResponse {
+
+        /** @var Student $user */
         $user = $security->getUser();
-        $examSheet = new ExaminationSheet();
-        $examSheet->setId($idGenerator->generateExaminationSheetId());
-        $examSheet->setStudent($user);
-        $examSheet->setExam($exam);
-        $examinationSheetRepository->add($examSheet);
-        $iterator = $exam->getQuestions()->getIterator();
-        $iterator->uasort(function(Question $a, Question $b) {
-            return $a->getOrd() <=> $b->getOrd();
-        });
-        $question = (new ArrayCollection(iterator_to_array($iterator)))->first();
         $now = new DateTime();
-        $answer = new Answer();
-        $answer->setStart($now);
-        $answer->setEnd($now);
-        $answer->setId($idGenerator->generateAnswerId());
-        $answer->setQuestion($question);
-        $answer->setExaminationSheet($examSheet);
-        $answerRepository->add($answer);
-        return new JsonResponse([
-            'examinationSheet' => $examSheet->getId(),
-            'answer' => $answer->getId()
-        ]);
+        return new JsonResponse($process->start($user, $exam, $now));
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    #[Route('/{exam}/next/{question}', name: 'app_exam_next', methods: ['GET'])]
+    public function next(
+        Exam $exam,
+        Question $question,
+        Security $security,
+        ExaminationProcess $process
+    ): JsonResponse {
+
+        /** @var Student $user */
+        $user = $security->getUser();
+        $now = new DateTime();
+        return new JsonResponse($process->next($user, $exam, $question, $now));
     }
 }
