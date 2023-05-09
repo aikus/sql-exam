c86dc4c66d86942a05c9f133bc42ac841ad0736c
Nick <Nikita_Germanov@mail.ru>
2023-05-10 01:24:34 +0500

    Code style #188

--- assets/pages/CreateCourse/CourseElementType/PollType.js
+++ assets/pages/CreateCourse/CourseElementType/PollType.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Editor } from "react-draft-wysiwyg";
 import { wysiwygConfig } from "/assets/config";
 import { CourseElementTitleInput } from "./Component/CourseElementTitleInput";
