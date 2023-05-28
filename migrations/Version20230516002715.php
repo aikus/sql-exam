88171649e1ecdd13785a239f8129e425beecfe60
Vladislav <tertyshnyiy.vladislav@gmail.com>
2023-05-28 15:07:54 +0500

    #206 Доработки визивига

--- migrations/Version20230516002715.php
+++ migrations/Version20230516002715.php
@@ -19,7 +19,7 @@ final class Version20230516002715 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE course_element ADD meta_type VARCHAR(255) AFTER type');
+        $this->addSql('ALTER TABLE course_element ADD meta_type VARCHAR(255) AFTER `type`');
 
     }
 
