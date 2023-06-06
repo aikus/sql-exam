38373efed074b511d9886aec67605654106aec78
Nick <Nikita_Germanov@mail.ru>
2023-06-07 01:48:41 +0500

    Курс может удалить только владелец или администратор #113

--- src/Connectors/CourseListener.php
+++ src/Connectors/CourseListener.php
@@ -3,7 +3,9 @@
 namespace App\Connectors;
 
 use App\Entity\Course;
+use App\Entity\User;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Security\Core\Security;
 
 class CourseListener
@@ -12,11 +14,21 @@ class CourseListener
     {
     }
 
-    public function prePersist(Course $course, LifecycleEventArgs $event)
+    public function prePersist(Course $course, LifecycleEventArgs $event): void
     {
         if(!$course->getCreator()){
             $course->setCreator($this->security->getUser());
         }
+    }
 
+    public function preRemove(Course $course, LifecycleEventArgs $event): void
+    {
+        $actor = $this->security->getUser();
+        if(
+            !in_array(User::ROLE_ADMIN, $actor->getRoles())
+            && $course->getCreator()->getUserIdentifier() !== $actor->getUserIdentifier()
+        ) {
+            throw new AccessDeniedHttpException('Вы не являетесь создателем курса или администратором');
+        }
     }
 }
