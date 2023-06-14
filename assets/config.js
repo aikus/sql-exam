062abf6566a533cc8e34e46313aac15558168404
Vladislav <tertyshnyiy.vladislav@gmail.com>
2023-06-14 23:12:55 +0500

    #220 адаптивное поле с подсветкой синтаксиса

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,5 @@
-export const hostName = ''
+export const hostName = '';
+export const syntaxFieldHeight = 150;
 
 export const wysiwygConfig = {
   options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji', 'image', 'remove', 'history'],
